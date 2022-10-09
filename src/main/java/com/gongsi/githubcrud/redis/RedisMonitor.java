f356c1088fd04b5bebd5c278f3da082d3c536489
zhangleilei <2430644326@qq.com>
2022-10-09 22:30:38 +0800

    springboot 使用redis监控key， 实现多线程的抢一个标

--- src/main/java/com/gongsi/githubcrud/redis/RedisMonitor.java
+++ src/main/java/com/gongsi/githubcrud/redis/RedisMonitor.java
@@ -13,7 +13,7 @@ import java.util.concurrent.Executors;
  * 2022/10/8
  */
 /*
-* 使用redis监控实现秒杀时的 防止超卖的现象
+* 使用redis监控实现多人投标时的有一人投中的现象
 * 使用时需打开redis服务器，windows中 redis.cli
 * */
 public class RedisMonitor {
@@ -29,7 +29,7 @@ public class RedisMonitor {
             e.printStackTrace();
         }
 
-        for (int i = 0; i < 1000; i++) {//尝试1000次
+        for (int i = 0; i < 10; i++) {//尝试1000次
             executorService.execute(() -> {
                 Jedis jedis1 = new Jedis("127.0.0.1", 6379);
                 try {
@@ -38,18 +38,18 @@ public class RedisMonitor {
                     int valInteger = Integer.valueOf(redisValue);
                     String userInfo = UUID.randomUUID().toString();
                     // 没有秒完
-                    if (valInteger < 20) {//redisKey
+                    if (valInteger < 1) {//redisKey
                         Transaction tx = jedis1.multi();//开启事务
                         tx.incr(redisKey);//自增
                         List list = tx.exec();//提交事务，如果返回nil则说明执行失败，因为我watch了的，只要执行失败，则
                         // 进来发现东西还有，秒杀成功
                         if (list != null && list.size() > 0) {
-                            System.out.println("用户：" + userInfo + "，秒杀成功！当前成功人数：" + (valInteger + 1));
+                            System.out.println("用户：" + userInfo + "，投标成功！当前成功人数：" + (valInteger + 1) + ThreadLocal.class.getSimpleName());
                         } else {//执行结果不是OK，说明被修改了，被别人抢了
-                            System.out.println("用户：" + userInfo + "，秒杀失败");
+                            System.out.println("用户：" + userInfo + "，投标失败");
                         }
                     } else {//东西秒完了
-                        System.out.println("已经有20人秒杀成功，秒杀结束");
+                        System.out.println("已经有1人投标成功，投标结束");
                     }
                 } catch (Exception e) {
                     e.printStackTrace();
