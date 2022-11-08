461415ee64803d87ca33396238f7245dbd178bb0
zhangleilei <2430644326@qq.com>
2022-11-09 07:56:29 +0800

    springboot 打包war包

--- src/main/java/com/gongsi/githubcrud/GithubCrudApplication.java
+++ src/main/java/com/gongsi/githubcrud/GithubCrudApplication.java
@@ -1,8 +1,11 @@
 package com.gongsi.githubcrud;
 
+import org.aspectj.weaver.ast.Var;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -13,7 +16,11 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement  //使具有事务管理的功能
 //不需要使用@ComponentScan，因为springboot默认扫描启动类所在同级目录下的所有类
 //@ComponentScan(basePackages = {"com.gongsi.githubcrud.conroller","com.gongsi.githubcrud.service"})
-public class GithubCrudApplication {
+/*
+* 因为需要打包成war包，所以需要继承SpringBootServletInitializer类，
+* 重写configure方法。
+* */
+public class GithubCrudApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(GithubCrudApplication.class, args);
@@ -21,4 +28,8 @@ public class GithubCrudApplication {
         System.out.println("bb");
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(GithubCrudApplication.class);
+    }
 }
--- src/main/java/com/gongsi/githubcrud/junit/JunitTest.java
+++ src/main/java/com/gongsi/githubcrud/junit/JunitTest.java
@@ -14,25 +14,49 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  * 2022/10/6
  */
 /*
-*使用单元测试  pom中spring-boot-starter-test依赖可使用@sprigbootTest注解与@Test注解
-* @SpringBootTest：@SpringBootTest后，Spring将加载所有被管理的bean，
-*                  基本等同于启动了整个服务，此时便可以开始功能测试
-* @Test：是该方法可以不用在main方法中调用就可以测试出运行结果的一种测试工具
-*        ，不过需要注意被测试的方法必须是public修饰的。
-* @RunWith()：一个运行器
-* SpringJUnit4ClassRunner.class:让测试运行于Spring测试环境 或 JUnit4.class:指用JUnit4来运行
-* 或Suite.class:一套测试集合
-* */
+ *使用单元测试  pom中spring-boot-starter-test依赖可使用@sprigbootTest注解与@Test注解
+ * @SpringBootTest：@SpringBootTest后，Spring将加载所有被管理的bean，
+ *                  基本等同于启动了整个服务，此时便可以开始功能测试
+ * @Test：是该方法可以不用在main方法中调用就可以测试出运行结果的一种测试工具
+ *        ，不过需要注意被测试的方法必须是public修饰的。
+ * @RunWith()：一个运行器
+ * SpringJUnit4ClassRunner.class:让测试运行于Spring测试环境 或 JUnit4.class:指用JUnit4来运行
+ * 或Suite.class:一套测试集合
+ * */
 @SpringBootTest
 @RunWith(SpringJUnit4ClassRunner.class)
 public class JunitTest {
     @Autowired
     UserServic userServic;
+
     @Test
-    public void ceShi(){
+    public void ceShi() {
         User user = userServic.find(1);
-        System.out.println("启用单元测试"+user);
+        System.out.println("启用单元测试" + user);
 
     }
-    
+
+    @Test
+    public void ceShiTest() {
+        int x = 4;
+        System.out.println(((x > 4) ? 99.9 : 9));
+        System.out.println(x);
+    }
+
+    @Test
+    public void ceShiTest1() {
+        int result = 0;
+        switch (2) {
+            case 1:
+                result = result + 2;
+            case 2:
+                result = result + 2 * 2;
+            case 3:
+                result = result + 2 * 3;
+        }
+        System.out.println(result);
+    }
+
+
+
 }
